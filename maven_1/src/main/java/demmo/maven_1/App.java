ecd7f1c5cbaacbbe83b5b0608f879342517763da
Pengjue <1759325396@qq.com>
2019-03-26 09:01:24 +0800

    maven_1

---
+++ maven_1/src/main/java/demmo/maven_1/App.java
@@ -0,0 +1,13 @@
+package demmo.maven_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
